42870e7ec8f7fb5307c5fe12ec0488672680fcc5
n4cr4 <itosukenao@gmail.com>
2023-06-22 10:30:17 +0900

    Remove nvim entry

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-ln -s ~/dotfiles/nvchad/custom ~/dotfiles/.config/nvim/lua/
-ln -s ~/dotfiles/.config/nvim ~/.config/
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
