e81818bb97c9530e53acef59d010ae510460b2f1
n4cr4 <itosukenao@gmail.com>
2024-01-06 21:04:33 +0900

    feat: use tmux

---
+++ .bin/install.bin.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+ln -s ~/dotfiles/.bin/dev /usr/local/bin/dev
--- .bin/install.sh
+++ .bin/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
 ln -s ~/dotfiles/.nvim/* ~/.config/nvim/
 ln -s ~/dotfiles/.gdbinit ~/.gdbinit
