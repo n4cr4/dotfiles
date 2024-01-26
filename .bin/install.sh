2ab39037bb85629e0d8e2564f6d132d1d540d8b9
n4cr4 <itosukenao@gmail.com>
2024-01-27 03:01:06 +0900

    feat: use lazy-nvim

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,5 +2,5 @@
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
-ln -s ~/dotfiles/.nvim/* ~/.config/nvim/
 ln -s ~/dotfiles/.gdbinit ~/.gdbinit
+ln -s ~/dotfiles/nvim ~/.config/
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,29 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+-- -- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Resize window
+keymap.set("n", "<C-w><C-h>", "<C-w><")
+keymap.set("n", "<C-w><C-l>", "<C-w>>")
+keymap.set("n", "<C-w><C-k>", "<C-w>+")
+keymap.set("n", "<C-w><C-j>", "<C-w>-")
