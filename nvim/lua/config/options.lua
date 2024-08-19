cbb211b05ac8a743cc8f9edb2b23fc190b8fa5aa
n4cr4 <itosukenao@gmail.com>
2024-08-19 11:43:16 +0900

    update: opt

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,5 +4,10 @@ vim.g.lazyvim_python_lsp = "pyright"
 local opt = vim.opt
 
 opt.spelllang = "en,cjk"
+<<<<<<< HEAD
 opt.wrap = true
 opt.conceallevel = 0
+=======
+opt.clipboard = "unnamedplus"
+opt.wrap = true
+>>>>>>> 8f0a96e (update: opt)
