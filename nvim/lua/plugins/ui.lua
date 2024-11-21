a61f6bbdb74ef2cff4c4d6489c5a90b6a11d15cc
n4cr4 <sukenao.ito@nflabs.jp>
2024-11-21 12:46:42 +0900

    fix: status bar

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,9 +1,18 @@
 return {
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 15000,
-      top_down = false,
-    },
-  },
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 15000,
+			top_down = false,
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				section_separators = { left = "", right = "" },
+				component_separators = "",
+			},
+		},
+	},
 }
