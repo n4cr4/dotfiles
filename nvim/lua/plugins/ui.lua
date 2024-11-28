87e89e0542916393a3ab4025cf5d2f9fd1f6fac9
n4cr4 <sukenao.ito@nflabs.jp>
2024-11-28 18:20:53 +0900

    feat: float (wip)

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -15,4 +15,39 @@ return {
 			},
 		},
 	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		-- keys = {
+		-- 	{
+		-- 		"<leader>fe",
+		-- 		function()
+		-- 			require("neo-tree.command").execute({
+		-- 				toggle = true,
+		-- 				dir = vim.uv.cwd(),
+		-- 				position = "float",
+		-- 			})
+		-- 		end,
+		-- 	},
+		-- 	{
+		-- 		"<leader>fE",
+		-- 		function()
+		-- 			require("neo-tree.command").execute({
+		-- 				toggle = true,
+		-- 				dir = LazyVim.root(),
+		-- 				position = "float",
+		-- 			})
+		-- 		end,
+		-- 	},
+		-- 	{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+		-- 	{ "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
+		-- },
+		opts = {
+			filesystem = {
+				always_show = {
+					".gitignore",
+					".env",
+				},
+			},
+		},
+	},
 }
